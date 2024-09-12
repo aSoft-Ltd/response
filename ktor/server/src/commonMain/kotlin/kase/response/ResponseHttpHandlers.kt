9cd8b96295abf122321be4004ee6653c585fd1ef
andylamax <andylamax@programmer.net>
2024-09-13 00:40:01 +0300

    Finished working on response

--- ktor/server/src/commonMain/kotlin/kase/response/ResponseHttpHandlers.kt
+++ ktor/server/src/commonMain/kotlin/kase/response/ResponseHttpHandlers.kt
@@ -1,31 +1,41 @@
 package kase.response
 
-import io.ktor.server.application.ApplicationCall
-import io.ktor.server.routing.*
-import io.ktor.util.pipeline.PipelineContext
+import io.ktor.server.routing.Routing
+import io.ktor.server.routing.RoutingContext
+import io.ktor.server.routing.delete
+import io.ktor.server.routing.get
+import io.ktor.server.routing.patch
+import io.ktor.server.routing.post
+import io.ktor.server.routing.put
 import kotlinx.serialization.StringFormat
 
 
-inline fun <reified D> Routing.post(
+inline fun Routing.post(
     path: String,
     codec: StringFormat,
-    noinline block: suspend RoutingContext.() -> D
+    noinline block: suspend RoutingContext.() -> String
 ) = post(path) { respondJson(codec) { block() } }
 
-inline fun <reified D> Routing.patch(
+inline fun Routing.put(
     path: String,
     codec: StringFormat,
-    noinline block: suspend RoutingContext.() -> D
+    noinline block: suspend RoutingContext.() -> String
+) = put(path) { respondJson(codec) { block() } }
+
+inline fun Routing.patch(
+    path: String,
+    codec: StringFormat,
+    noinline block: suspend RoutingContext.() -> String
 ) = patch(path) { respondJson(codec) { block() } }
 
-inline fun <reified D> Routing.delete(
+inline fun Routing.delete(
     path: String,
     codec: StringFormat,
-    noinline block: suspend RoutingContext.() -> D
+    noinline block: suspend RoutingContext.() -> String
 ) = delete(path) { respondJson(codec) { block() } }
 
-inline fun <reified D> Routing.get(
+inline fun Routing.get(
     path: String,
     codec: StringFormat,
-    noinline block: suspend RoutingContext.() -> D
+    noinline block: suspend RoutingContext.() -> String
 ) = get(path) { respondJson(codec) { block() } }
